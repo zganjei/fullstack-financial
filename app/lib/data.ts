8d60964fa3b0619e9a5ad12b61fedf37044ce73e
Zeinab Ganjei <zeinab.ganjei@ericsson.com>
2025-04-10 14:30:51 +0200

    loading and suspense added to dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,15 +13,8 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
