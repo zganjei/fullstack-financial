006456066663184eb54b724e0e905726c98d1e29
Zeinab Ganjei <zzganjei@gmail.com>
2025-04-11 15:20:50 +0200

    login added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import {z} from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!,{ssl:'require'});
 
@@ -94,4 +96,23 @@ export async function deleteInvoice(id: string) {
         console.error('Error deleting invoice items:', error);
     }
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+){
+    try{
+        await signIn('credentials', formData);
+    } catch(error){
+        if (error instanceof AuthError){
+            switch (error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
