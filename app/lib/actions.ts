5b1b5a122e7bf075e43970641cae464d073c515d
Zeinab Ganjei <zzganjei@gmail.com>
2025-04-11 12:15:37 +0200

    form validation added to create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,20 +8,42 @@ const sql = postgres(process.env.POSTGRES_URL!,{ssl:'require'});
 
 const FormSchema = z.object({
     id:z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending','paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, {message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending','paid'],{
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     data:z.string(),
 });
 
+export type State ={
+    errors?:{
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
 const CreateInvoice = FormSchema.omit({id: true, data:true});
 
-export async function createInvoice(formData: FormData){
-    const {customerId, amount, status} = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData){
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+    if (!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.'
+        };
+    }
+    const {customerId, amount, status} = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
     try{
@@ -30,7 +52,9 @@ export async function createInvoice(formData: FormData){
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error){    
-        console.error(error);
+        return{
+            message: 'Database Error: Failed to Create Invoice.',
+        };
     }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -61,7 +85,6 @@ export async function updateInvoice(id:string, formData: FormData){
 };
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Not implemented');
     try{
         await sql`
             DELETE FROM invoice_items
